901bddad6d440da46ced3f5c81e7036ed50ee3e4
Brett Roeder <74941938+mirewolf@users.noreply.github.com>
2021-05-21 16:01:54 -0700

    good stuff

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
